f3c0fbb8fd71e596f0ed586d39aaea349bd59091
LucasAnSilvs <lucas.antonio@webjump.com.br>
2023-09-19 17:17:41 -0300

    feat: typing styled components

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from '@routes/app.routes';
+import theme from '@theme/index';
+import { ThemeProvider } from 'styled-components/native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
--- app/src/routes/app.routes.tsx
+++ app/src/routes/app.routes.tsx
@@ -5,6 +5,8 @@ import {
 import { Favorites } from '@screens/Favorites';
 import { Movies } from '@screens/Movies';
 import { SelectedMovie } from '@screens/SelectedMovie';
+import { Queue, Star } from 'phosphor-react-native';
+import { useTheme } from 'styled-components/native';
 
 import Route from './enums';
 
@@ -19,11 +21,36 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
 const Tab = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
+  const theme = useTheme();
+
   return (
-    <Tab.Navigator>
-      <Tab.Screen name={Route.MOVIES} component={Movies} />
-      <Tab.Screen name={Route.FAVORITES} component={Favorites} />
-      <Tab.Screen name={Route.SELECTED_MOVIE} component={SelectedMovie} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tab.Screen
+        name={Route.MOVIES}
+        component={Movies}
+        options={{
+          tabBarIcon: () => (
+            <Queue color={theme.colors.red[400]} weight="fill" size={32} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={Route.FAVORITES}
+        component={Favorites}
+        options={{
+          tabBarIcon: () => <Star color="#ff5757" weight="fill" size={32} />,
+        }}
+      />
+      <Tab.Screen
+        name={Route.SELECTED_MOVIE}
+        component={SelectedMovie}
+        options={{ tabBarButton: () => null }}
+      />
     </Tab.Navigator>
   );
 }
